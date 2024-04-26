4fa5f92804778d18e6dca1ae9e2f924d405a1dd2
Nurudeen Hamzah <hamdeen247@gmail.com>
2024-04-26 17:11:38 +0000

    creating modules

---
+++ searchUpdate.js
@@ -0,0 +1,8 @@
+const url = "https//:updates.io"; // dummy endpoint
+
+const searchUpdate = (files) => {
+  // send http request
+  console.log("update available");
+}
+
+module.exports.searchUpdate = searchUpdate;
