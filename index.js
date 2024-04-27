afc4d17db581c3d378f8b11bf30a7c9da1cf55ad
Nurudeen Hamzah <hamdeen247@gmail.com>
2024-04-27 06:58:36 +0000

    install types/node

--- index.js
+++
@@ -1,8 +0,0 @@
-const path = require('path')  //* working with paths, instead of using strings
-
-
-
-const pathObj = path.parse(__filename)
-console.log(pathObj.name)
-console.log(path.extname(__filename))
-console.log(__filename)
