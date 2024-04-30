7cdb5234135aaca1d0a244581129f3535192c8b2
Nurudeen Hamzah <hamdeen247@gmail.com>
2024-04-30 07:33:59 +0000

    working with env variables

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express();
+const port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
     res.send("Hello world")
@@ -13,4 +14,4 @@ app.get('/api/courses', (req, res) => {
     ]))
 })
 
-app.listen(4000, () => console.log('Listening on port 4000...'))
+app.listen(port, () => console.log(`Listening on port ${port}...`))
