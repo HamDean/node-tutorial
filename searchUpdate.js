fe6e51afd711535c98848c891a568bab4cb6d76e
Nurudeen Hamzah <hamdeen247@gmail.com>
2024-04-30 07:12:08 +0000

    working with express - handling get request

--- searchUpdate.js
+++
@@ -1,9 +0,0 @@
-const url = "https//:updates.io"; // dummy endpoint
-
-const searchUpdate = (file) => {
-  // send http request
-  console.log("update available for", file);
-}
-
-// module.exports.searchUpdate = searchUpdate; // exporting an object
-module.exports = searchUpdate
