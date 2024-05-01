819ce1a149563da7d5462394f3a35802eeef065f
Nurudeen Hamzah <hamdeen247@gmail.com>
2024-05-01 08:10:21 +0000

    working with 3-party middleware

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const Joi = require("joi");
 const logger = require("./logger");
+const morgan = require("morgan");
 const authenticator = require("./authenticator");
 const app = express();
 const port = process.env.PORT || 4000;
@@ -13,6 +14,8 @@ app.use(express.static("public"));
 
 //* parsing url encoded request
 app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan('short'));
 
 app.use(logger);
 
