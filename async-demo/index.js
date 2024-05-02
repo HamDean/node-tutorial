19a94c8241f61010184acb726df72604c88f351c
Nurudeen Hamzah <hamdeen247@gmail.com>
2024-05-02 16:33:19 +0000

    working with callbacks

---
+++ async-demo/index.js
@@ -0,0 +1,23 @@
+console.log("Before");
+
+fetchUser(1, (user) => {
+  fetchReposities(user.userName, (repos) => {
+    console.log("Repositories:", repos);
+  });
+});
+
+console.log("After");
+
+function fetchUser(id, callback) {
+  setTimeout(() => {
+    console.log("Fetching user from the database...");
+    callback({ id: id, userName: "hamDeen" });
+  }, 2000);
+}
+
+function fetchReposities(username, callback) {
+  setTimeout(() => {
+    console.log('Fetching repos...')
+    callback(["repo1", "repo2", "repo3"]);
+  }, 2000);
+}
